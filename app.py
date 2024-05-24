df862d210b609c6014efb46b0311fb8cb75e558f
alielsonfp <alielson_pequeno@hotmail.com>
2024-05-24 18:27:28 -0300

    add Logout feature

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 from models.user import User
 from database import db
-from flask_login import LoginManager, login_user, current_user
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required
 
 app = Flask (__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
@@ -35,6 +35,11 @@ def login():
     return jsonify({"message": "Credenciais invalidas"}), 400
 
 
+@app.route("/logout", methods=["GET"])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"message": "Logout realizado com sucesso!"})
 
 @app.route("/hello-world", methods=["GET"])
 def hello_world():
