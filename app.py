64e9c6030999bce514e7911052071d50f5823882
alielsonfp <alielson_pequeno@hotmail.com>
2024-05-24 17:19:16 -0300

    add database.py, user.py and login manager

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify, request
+from models.user import User
+from database import db
+from flask_login import LoginManager
+
+app = Flask (__name__)
+app.config['SECRET_KEY'] = "your_secret_key"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
+
+login.manager = LoginManager()
+db.init_app(app)
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+        pass
+        #login
+    return jsonify({"message:" "Credenciais invalidas"}), 400
+
+
+
+@app.route("/hello-world", methods=["GET"])
+def hello_world():
+    return "Hello world"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
